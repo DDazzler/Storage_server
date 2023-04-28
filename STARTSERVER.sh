7d7395dea9c7f715e7b9f9c07c05afb841b30904
ddazzler <deepak.maretha@webkorps.com>
2023-04-28 23:00:31 +0530

    Update

--- STARTSERVER.sh
+++ STARTSERVER.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-#echo "FILE SERVER IS STARTING PLEASE WAIT"
-#echo "-----------------------------------------------"
-#screen -dmS FILESERVER bash -c "cd /home/server/ALL\ SERVER\ CODES/file-server-code/ && rails s -p 3050 -b 192.168.0.100"
-
-#!/bin/bash
-
 echo "FILE SERVER IS STARTING PLEASE WAIT"
 echo "-----------------------------------------------"
+screen -dmS FILESERVER bash -c "cd /home/server/ALL\ SERVER\ CODES/file-server-code/ && rails s -p 3050 -b 192.168.0.100"
 
-nohup rails s -p 3050 -b 192.168.0.100 > server.log 2>&1 &
+
