b00e13a538915e4fd92bf3c7f4f2e4acddcd5f32
ddazzler <deepak.maretha@webkorps.com>
2023-05-09 18:41:17 +0530

    COMMIT FROM SERVER MACHINE

--- STARTSERVER.sh
+++ STARTSERVER.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "FILE SERVER IS STARTING PLEASE WAIT"
 echo "-----------------------------------------------"
-screen -dmS FILESERVER bash -c "cd /home/server/ALL\ SERVER\ CODES/file-server-code/ && rails s -p 3050 -b 192.168.0.100"
-
+screen -dmS FILESERVER bash -c "cd /home/server/SERVERS/file-server-code/ && rails s -p 3050 -b 192.168.0.100"
 
